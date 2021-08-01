c09d316d6d52969b523c9f2d7d2ad6e738b00fee
Mantas Antanaitis <antanaitis.web@gmail.com>
2021-08-01 14:08:17 +0300

    first commit

---
+++ src/components/use-dark-mode.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function useDarkMode() {
+  const [theme, setTheme] = useState(
+    typeof window !== "undefined" ? window.localStorage.getItem('theme') : "light"
+  );
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(theme === "light" ? "theme-dark" : "theme-light");
+    root.classList.add(theme === "light" ? "theme-light" : "theme-dark");
+
+    if (typeof window !== "undefined") {
+      localStorage.setItem("theme", theme);
+    }
+  }, [theme]);
+
+  return [theme, setTheme];
+}
+
+export default useDarkMode;
